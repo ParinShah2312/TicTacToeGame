1328386309290116c18787b9471572601a6e986b
Parin Shah <shahparin2312@gmail.com>
2023-06-05 18:21:17 +0530

    coding completed

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,11 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        <span className="text-orange">TIC</span>
+        <span className="text-green"> TAC</span>
+        <span className="text-orange"> TOE</span>
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -73,7 +78,13 @@ function App() {
       >
         Start New Game
       </button>
-      <h2>Current Game History</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current Game History
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
